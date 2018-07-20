f8735bdf117de69f68805b3674f7590c19dfeead
Xavier Denis <xldenis@gmail.com>
2018-07-20 13:55:09 -0500

    Get add instruction working

--- besm-vm/src/main.rs
+++ besm-vm/src/main.rs
@@ -17,6 +17,7 @@ struct VM<'a> {
   local_ic: u16,
   global_ic: u16,
   active_ic: ActiveIC,
+  stopped: bool,
 }
 
 enum ActiveIC { Global, Local}
@@ -28,27 +29,28 @@ impl<'a> VM<'a> {
     VM {
       is: is,
       ds: &DS,
-      global_ic: 0,
-      local_ic: 0,
-      active_ic: ActiveIC::Global
+      global_ic: 1,
+      local_ic: 1,
+      active_ic: ActiveIC::Global,
+      stopped: false,
     }
   }
 
-  pub fn step(&'a mut self) -> () {
-    let opcode = self.is[self.next_instr() as usize];
+  pub fn step(&mut self) -> () {
+    let opcode = self.is[self.next_instr() as usize - 1];
 
     match Instruction::from_bytes(opcode) {
       Add  { a: l, b: r, c: res, normalize: needs_norm } => {
-        let mut lfloat = Float::from_bytes(l);
-        let mut rfloat = Float::from_bytes(r);
+        let mut lfloat = Float::from_bytes(self.get_address(l));
+        let mut rfloat = Float::from_bytes(self.get_address(r));
 
         match lfloat.exp.cmp(&rfloat.exp) {
           Ordering::Less    => {
-            lfloat.mant >>= lfloat.exp - rfloat.exp;
+            lfloat.mant = lfloat.mant.checked_shr((lfloat.exp - rfloat.exp) as u32).unwrap_or(0);
             lfloat.exp = rfloat.exp;
           }
           Ordering::Greater => {
-            rfloat.mant >>= rfloat.exp - lfloat.exp;
+            rfloat.mant = rfloat.mant.checked_shr((rfloat.exp - lfloat.exp) as u32).unwrap_or(0);
             rfloat.exp = lfloat.exp;
           }
           Ordering::Equal => { }
@@ -64,8 +66,8 @@ impl<'a> VM<'a> {
         let mut val = Float { mant: res_mant, overflow: overflow, sign: res_sign, exp: rfloat.exp };
 
         if needs_norm { val.normalize() };
-
-        self.set_address(res, val.to_bytes()).increment_ic();
+        self.set_address(res, val.to_bytes());
+        self.increment_ic();
 
       }
       CCCC { b: cell, c: addr } => {
@@ -84,18 +86,18 @@ impl<'a> VM<'a> {
       }
       Stop => {}
       Stop28 => {}
-      _a => {}
+      _a => { self.stopped = true; }
     }
   }
 
-  fn next_instr(&'a self) -> u16 {
+  fn next_instr(& self) -> u16 {
     match &self.active_ic {
       ActiveIC::Global => self.global_ic,
       ActiveIC::Local  => self.local_ic,
     }
   }
 
-  fn increment_ic(&'a mut self) -> () {
+  fn increment_ic(&mut self) -> () {
     match &self.active_ic {
       ActiveIC::Global => self.global_ic += 1,
       ActiveIC::Local  => self.local_ic += 1,
@@ -103,7 +105,7 @@ impl<'a> VM<'a> {
 
   }
 
-  fn get_address(&'a self, ix: u64) -> u64 {
+  fn get_address(& self, ix: u64) -> u64 {
     if ix <= 1023 {
       self.is[(ix - 1) as usize]
     } else {
@@ -111,7 +113,7 @@ impl<'a> VM<'a> {
     }
   }
 
-  fn set_address(&'a mut self, ix: u64, val: u64) -> &mut Self {
+  fn set_address(& mut self, ix: u64, val: u64) -> &mut Self {
     if ix <= 1023 {
       self.is[(ix - 1) as usize] = val
     } else {
@@ -122,6 +124,7 @@ impl<'a> VM<'a> {
  }
 extern crate num;
 
+#[derive(Debug)]
 struct Float {
   mant: u32, // actually only the lower 32 bits are used (u64 lets us store overflow  though...)
   overflow: bool,
@@ -132,16 +135,14 @@ struct Float {
 impl Float {
   pub fn from_bytes(word: u64) -> Float {
     let exp = (0xFF & word.get_bits(33..38)) as i8;
-    let mant = word.get_bits(0..31) as u32;
+    let mant = word.get_bits(0..32) as u32;
     let sign = word.get_bit(32);
-
     Float { mant: mant, exp: exp, sign: sign, overflow: false }
   }
 
   pub fn to_bytes(&mut self) -> u64 {
     let mut bytes : u64 = 0;
-
-    bytes.set_bits(0..31, self.mant as u64);
+    bytes.set_bits(0..32, self.mant.into());
     bytes.set_bit(32, self.sign);
     *bytes.set_bits(33..38, self.exp as u64) // oh boy is this correct?
   }
@@ -154,8 +155,9 @@ impl Float {
     } else {
       let mut shift = 0;
       while self.mant.get_bit(31) == false {
+
         shift += 1;
-        self.mant << 1;
+        self.mant <<= 1;
       }
 
       self.exp  = num::clamp(self.exp - shift, -32,  31);
@@ -165,6 +167,7 @@ impl Float {
 
 type Address = u64;
 
+#[derive(Debug)]
 enum Instruction {
   Add       { a: Address, b: Address, c: Address, normalize: bool},
   Sub       { a: Address, b: Address, c: Address, normalize: bool},
@@ -203,36 +206,36 @@ enum Instruction {
 
 use Instruction::*;
 
-fn third_addr(x: u64) -> u64 {
-  x.get_bits(22..31)
+fn first_addr(x: u64) -> u64 {
+  x.get_bits(22..32)
 }
 fn second_addr(x: u64) -> u64 {
-  x.get_bits(22..31)
+  x.get_bits(11..21)
 }
-fn first_addr(x: u64) -> u64 {
+fn third_addr(x: u64) -> u64 {
   x.get_bits(0..10)
 }
 
 impl Instruction {
   pub fn from_bytes(word: u64) -> Instruction {
-    match (word.get_bits(32..37)) {
-      0x01 => Add       { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x02 => Sub       { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x03 => Mult      { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x04 => Div       { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x05 => AddE      { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x06 => SubE      { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x07 => Ce        { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x08 => Xa        { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x09 => Xb        { normalize: word.get_bit(38),                                            c: third_addr(word)},
-      0x0A => DivA      { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x0B => DivB      { normalize: word.get_bit(38),                                            c: third_addr(word)},
-      0x2B => TN        { normalize: word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
-      0x0C => PN        {                              a: first_addr(word)},
-      0x0D => TMin      { normalize: word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
-      0x0E => TMod      { normalize: word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
-      0x0F => TSign     { normalize: word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
-      0x10 => TExp      { normalize: word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
+    match word.get_bits(33..37) {
+      0x01 => Add       { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x02 => Sub       { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x03 => Mult      { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x04 => Div       { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x05 => AddE      { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x06 => SubE      { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x07 => Ce        { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x08 => Xa        { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x09 => Xb        { normalize: !word.get_bit(38),                                            c: third_addr(word)},
+      0x0A => DivA      { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x0B => DivB      { normalize: !word.get_bit(38),                                            c: third_addr(word)},
+      0x2B => TN        { normalize: !word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
+      0x0C => PN        {                               a: first_addr(word)},
+      0x0D => TMin      { normalize: !word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
+      0x0E => TMod      { normalize: !word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
+      0x0F => TSign     { normalize: !word.get_bit(38), a: first_addr(word), b: second_addr(word), c: third_addr(word)},
+      0x10 => TExp      { normalize: !word.get_bit(38), a: first_addr(word),                       c: third_addr(word)},
       0x11 => match word.get_bit(38) {
         false => Shift  {                              a: first_addr(word), b: second_addr(word), c: third_addr(word)},
         true => ShiftAll{                              a: first_addr(word), b: second_addr(word), c: third_addr(word)},
@@ -267,19 +270,27 @@ struct Opts {
   is_file: PathBuf,
 }
 
-use byteorder::{NativeEndian, ReadBytesExt};
+use byteorder::{BigEndian, ReadBytesExt};
 use std::iter;
 fn main() {
     let opt = Opts::from_args();
 
     let mut f = File::open(opt.is_file.clone()).expect("file not found");
 
-    let words : Vec<u64> = iter::repeat_with(|| f.read_u64::<NativeEndian>().unwrap()).take(1023).collect();
+    let words : Vec<u64> = iter::repeat_with(|| f.read_u64::<BigEndian>().unwrap_or(0)).take(1023).collect();
     let mut is_buf = [0u64; 1023];
     is_buf.copy_from_slice(&words[..]);
 
     let mut vm = VM::new(&mut is_buf);
-    vm.step();
+
+    while !vm.stopped {
+      let ins = vm.is[vm.next_instr() as usize];
+
+      println!("{:?} {:039b} {:016x}", vm.next_instr(), ins, ins);
+      vm.step();
+
+    }
+
     println!("{:?}", opt);
 
 }
