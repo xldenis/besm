6534c56f435c62657044ccbfd5f31a6ec2981a45
Xavier Denis <xldenis@gmail.com>
2018-08-29 15:23:17 -0500

    Formatting

--- besm-vm/src/float.rs
+++ besm-vm/src/float.rs
@@ -4,107 +4,133 @@ use std::cmp::*;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Float {
-  pub mant: u32, // actually only the lower 32 bits are used (u64 lets us store overflow  though...)
-  pub overflow: bool,
-  pub exp: i8, // actually is only 6 bits long aka (-32 - 31)
-  pub sign: bool
+    pub mant: u32, // actually only the lower 32 bits are used (u64 lets us store overflow  though...)
+    pub overflow: bool,
+    pub exp: i8, // actually is only 6 bits long aka (-32 - 31)
+    pub sign: bool,
 }
 
 macro_rules! equalize_exponents_shift_mant {
-  ($left:expr, $right:expr, $left_mant:ident, $right_mant:ident, $exp:ident) => {
-    let mut $left_mant:u32  = $left.mant;
-    let mut $right_mant:u32 = $right.mant;
-    let left_exp:i8   = $left.exp;
-    let right_exp:i8  = $right.exp;
+    ($left:expr, $right:expr, $left_mant:ident, $right_mant:ident, $exp:ident) => {
+        let mut $left_mant: u32 = $left.mant;
+        let mut $right_mant: u32 = $right.mant;
+        let left_exp: i8 = $left.exp;
+        let right_exp: i8 = $right.exp;
 
-
-    let $exp = match left_exp.cmp(&right_exp) {
-      Ordering::Less    => {
-        $left_mant = $left_mant.checked_shr((left_exp - right_exp) as u32).unwrap_or(0);
-        right_exp
-      }
-      Ordering::Greater => {
-        $right_mant = $right_mant.checked_shr((left_exp - right_exp) as u32).unwrap_or(0);
-        left_exp
-      }
-      Ordering::Equal => { left_exp }
+        let $exp = match left_exp.cmp(&right_exp) {
+            Ordering::Less => {
+                $left_mant = $left_mant
+                    .checked_shr((left_exp - right_exp) as u32)
+                    .unwrap_or(0);
+                right_exp
+            }
+            Ordering::Greater => {
+                $right_mant = $right_mant
+                    .checked_shr((left_exp - right_exp) as u32)
+                    .unwrap_or(0);
+                left_exp
+            }
+            Ordering::Equal => left_exp,
+        };
     };
-  };
 }
 
 impl Float {
-  pub fn from_bytes(word: u64) -> Float {
-    let exp = (0xFF & word.get_bits(33..39)) as i8;
-    let mant = word.get_bits(0..33) as u32;
-    let sign = word.get_bit(32);
-    Float { mant, exp, sign: sign, overflow: false }
-  }
-
-  pub fn new(mant: u32, exp: i8,) -> Float {
-    Float { mant, exp, overflow: false, sign: false}
-  }
-
-  pub fn to_bytes(&mut self) -> u64 {
-    let mut bytes : u64 = 0;
-    bytes.set_bits(0..32, self.mant.into());
-    bytes.set_bit(32, self.sign);
-    *bytes.set_bits(33..39, (self.exp & 0b111111) as u64) // oh boy is this correct?
-  }
-
-  pub fn normalize(&mut self) {
-    if self.overflow {
-      self.exp += 1;
-      self.mant >>= 1;
-      self.mant.set_bit(31, true);
-      self.overflow = false
-
-    } else if self.mant == 0 && self.exp == -32 {
-      ()
-    } else {
-      let mut shift = 0;
-      while self.mant.get_bit(31) == false && shift < 32 {
-
-        shift += 1;
-        self.mant <<= 1;
-      }
-
-      self.exp  = num::clamp(self.exp - shift, -32,  31);
+    pub fn from_bytes(word: u64) -> Float {
+        let exp = (0xFF & word.get_bits(33..39)) as i8;
+        let mant = word.get_bits(0..33) as u32;
+        let sign = word.get_bit(32);
+        Float {
+            mant,
+            exp,
+            sign: sign,
+            overflow: false,
+        }
     }
-  }
 
-  pub fn add_unnormalized(&self, other: &Float) -> Float {
-    equalize_exponents_shift_mant!(self, other, left_mant, right_mant, exp);
+    pub fn new(mant: u32, exp: i8) -> Float {
+        Float {
+            mant,
+            exp,
+            overflow: false,
+            sign: false,
+        }
+    }
 
-    let (res_mant, overflow) = left_mant.overflowing_add(right_mant);
-    let res_sign = match left_mant.cmp(&right_mant) {
-      Ordering::Less => other.sign,
-      Ordering::Greater => self.sign,
-      Ordering::Equal   => false,
-    };
+    pub fn to_bytes(&mut self) -> u64 {
+        let mut bytes: u64 = 0;
+        bytes.set_bits(0..32, self.mant.into());
+        bytes.set_bit(32, self.sign);
+        *bytes.set_bits(33..39, (self.exp & 0b111111) as u64) // oh boy is this correct?
+    }
 
-    Float { mant: res_mant, overflow: overflow, sign: res_sign, exp }
-  }
+    pub fn normalize(&mut self) {
+        if self.overflow {
+            self.exp += 1;
+            self.mant >>= 1;
+            self.mant.set_bit(31, true);
+            self.overflow = false
+        } else if self.mant == 0 && self.exp == -32 {
+            ()
+        } else {
+            let mut shift = 0;
+            while self.mant.get_bit(31) == false && shift < 32 {
+                shift += 1;
+                self.mant <<= 1;
+            }
 
-  pub fn sub_unnormalized(&self, other: &Float) -> Float {
-    equalize_exponents_shift_mant!(self, other, left_mant, right_mant, exp);
+            self.exp = num::clamp(self.exp - shift, -32, 31);
+        }
+    }
 
-    let (res_mant, overflow) = left_mant.overflowing_sub(right_mant);
-    let res_sign = match overflow {
-      true  => !other.sign,
-      false => self.sign,
-    };
+    pub fn add_unnormalized(&self, other: &Float) -> Float {
+        equalize_exponents_shift_mant!(self, other, left_mant, right_mant, exp);
 
-    Float { mant: res_mant, overflow: false, sign: res_sign, exp }
-  }
+        let (res_mant, overflow) = left_mant.overflowing_add(right_mant);
+        let res_sign = match left_mant.cmp(&right_mant) {
+            Ordering::Less => other.sign,
+            Ordering::Greater => self.sign,
+            Ordering::Equal => false,
+        };
 
-  // what is un normalized floating point multiplication?
-  pub fn mul_unnormalized(&self, other: &Float) -> Float {
-    equalize_exponents_shift_mant!(self, other, left_mant, right_mant, exp_half);
+        Float {
+            mant: res_mant,
+            overflow: overflow,
+            sign: res_sign,
+            exp,
+        }
+    }
 
-    let exp:i8 = exp_half + exp_half;
+    pub fn sub_unnormalized(&self, other: &Float) -> Float {
+        equalize_exponents_shift_mant!(self, other, left_mant, right_mant, exp);
 
-    let res_mant = u64::from(left_mant) * u64::from(right_mant);
+        let (res_mant, overflow) = left_mant.overflowing_sub(right_mant);
+        let res_sign = match overflow {
+            true => !other.sign,
+            false => self.sign,
+        };
 
-    Float { mant: res_mant.get_bits(32..64) as u32, overflow: false, sign: self.sign ^ other.sign, exp}
-  }
+        Float {
+            mant: res_mant,
+            overflow: false,
+            sign: res_sign,
+            exp,
+        }
+    }
+
+    // what is un normalized floating point multiplication?
+    pub fn mul_unnormalized(&self, other: &Float) -> Float {
+        equalize_exponents_shift_mant!(self, other, left_mant, right_mant, exp_half);
+
+        let exp: i8 = exp_half + exp_half;
+
+        let res_mant = u64::from(left_mant) * u64::from(right_mant);
+
+        Float {
+            mant: res_mant.get_bits(32..64) as u32,
+            overflow: false,
+            sign: self.sign ^ other.sign,
+            exp,
+        }
+    }
 }
--- besm-vm/src/main.rs
+++ besm-vm/src/main.rs
@@ -1,151 +1,177 @@
 #![feature(nll)]
 
 extern crate bit_field;
-#[macro_use] extern crate structopt;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate structopt;
+#[macro_use]
+extern crate log;
 
-extern crate tui_logger;
+extern crate arraydeque;
 extern crate byteorder;
 extern crate num;
-extern crate arraydeque;
 extern crate tui;
+extern crate tui_logger;
 
+use std::fs::File;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use std::fs::File;
 
-use vm::*;
-use interface::*;
 use byteorder::{BigEndian, ReadBytesExt};
+use interface::*;
 use std::iter;
+use vm::*;
 
 use tui::backend::MouseBackend;
 use tui::Terminal;
 
-
+use arraydeque::ArrayDeque;
 use std::io;
-use std::{thread, time};
-use arraydeque::{ArrayDeque};
 use std::sync::mpsc;
+use std::{thread, time};
 
 mod float;
-mod vm;
 mod interface;
+mod vm;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "besm-vm")]
 struct Opts {
-  #[structopt(short = "s", long = "start-address", default_value = "1")]
-  start_address: u64,
-  #[structopt(name = "FILE", parse(from_os_str))]
-  is_file: PathBuf,
+    #[structopt(short = "s", long = "start-address", default_value = "1")]
+    start_address: u64,
+    #[structopt(name = "FILE", parse(from_os_str))]
+    is_file: PathBuf,
 
-  #[structopt(long = "md0", parse(from_os_str))]
-  md0: Option<PathBuf>,
-  #[structopt(long = "md1", parse(from_os_str))]
-  md1: Option<PathBuf>,
-  #[structopt(long = "md2", parse(from_os_str))]
-  md2: Option<PathBuf>,
-  #[structopt(long = "md3", parse(from_os_str))]
-  md3: Option<PathBuf>,
-  #[structopt(long = "md4", parse(from_os_str))]
-  md4: Option<PathBuf>,
+    #[structopt(long = "md0", parse(from_os_str))]
+    md0: Option<PathBuf>,
+    #[structopt(long = "md1", parse(from_os_str))]
+    md1: Option<PathBuf>,
+    #[structopt(long = "md2", parse(from_os_str))]
+    md2: Option<PathBuf>,
+    #[structopt(long = "md3", parse(from_os_str))]
+    md3: Option<PathBuf>,
+    #[structopt(long = "md4", parse(from_os_str))]
+    md4: Option<PathBuf>,
 }
-
 
 extern crate termion;
 
 fn md_from_file(file: Option<PathBuf>) -> MagDrive {
-  let mut buf = [0; 1024];
-  match file {
-    None => { }
-    Some(path) => {
-      let mut f = File::open(path).expect("file not found");
-      let     words : Vec<u64> = iter::repeat_with(|| f.read_u64::<BigEndian>().unwrap_or(0)).take(1024).collect();
+    let mut buf = [0; 1024];
+    match file {
+        None => {}
+        Some(path) => {
+            let mut f = File::open(path).expect("file not found");
+            let words: Vec<u64> = iter::repeat_with(|| f.read_u64::<BigEndian>().unwrap_or(0))
+                .take(1024)
+                .collect();
 
-      buf.copy_from_slice(&words);
+            buf.copy_from_slice(&words);
+        }
     }
-  }
 
-  MagDrive::new(buf)
+    MagDrive::new(buf)
 }
 use log::LevelFilter;
 use tui_logger::*;
 
 fn main() {
-  init_logger(LevelFilter::Info).unwrap();
-  set_default_level(LevelFilter::Trace);
+    init_logger(LevelFilter::Info).unwrap();
+    set_default_level(LevelFilter::Trace);
 
-  let     opt = Opts::from_args();
-  let mut f = File::open(opt.is_file.clone()).expect("file not found");
-  let     words : Vec<u64> = iter::repeat_with(|| f.read_u64::<BigEndian>().unwrap_or(0)).take(1023).collect();
+    let opt = Opts::from_args();
+    let mut f = File::open(opt.is_file.clone()).expect("file not found");
+    let words: Vec<u64> = iter::repeat_with(|| f.read_u64::<BigEndian>().unwrap_or(0))
+        .take(1023)
+        .collect();
 
-  let mut is_buf = [0u64; 1023];
-  let mut terminal = Terminal::new(MouseBackend::new().unwrap()).unwrap();
+    let mut is_buf = [0u64; 1023];
+    let mut terminal = Terminal::new(MouseBackend::new().unwrap()).unwrap();
 
-  let mut interface = Interface {
-    size: terminal.size().unwrap(),
-    past_instrs: ArrayDeque::new(),
-  };
+    let mut interface = Interface {
+        size: terminal.size().unwrap(),
+        past_instrs: ArrayDeque::new(),
+    };
 
-  is_buf.copy_from_slice(&words[..]);
+    is_buf.copy_from_slice(&words[..]);
 
-  let mut x = [md_from_file(opt.md0), md_from_file(opt.md1), md_from_file(opt.md2), md_from_file(opt.md3), md_from_file(opt.md4)];
-  let mut y = [MagTape::new(), MagTape::new(), MagTape::new(), MagTape::new()];
-  let mut vm = VM::new(&mut is_buf, &mut x, &mut y, opt.start_address as u16);
+    let mut x = [
+        md_from_file(opt.md0),
+        md_from_file(opt.md1),
+        md_from_file(opt.md2),
+        md_from_file(opt.md3),
+        md_from_file(opt.md4),
+    ];
+    let mut y = [
+        MagTape::new(),
+        MagTape::new(),
+        MagTape::new(),
+        MagTape::new(),
+    ];
+    let mut vm = VM::new(&mut is_buf, &mut x, &mut y, opt.start_address as u16);
 
-  let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
-  thread::spawn(move || {
-    use termion::input::TermRead;
-    use termion::event;
+    thread::spawn(move || {
+        use termion::event;
+        use termion::input::TermRead;
 
-    let stdin = io::stdin();
+        let stdin = io::stdin();
 
-    for e in stdin.keys() {
-      let evt = e.unwrap();
+        for e in stdin.keys() {
+            let evt = e.unwrap();
 
-      tx.send(evt).unwrap();
+            tx.send(evt).unwrap();
 
-      if let event::Key::Char('q') = evt { break; }
+            if let event::Key::Char('q') = evt {
+                break;
+            }
+        }
+    });
+
+    terminal.clear().unwrap();
+    terminal.hide_cursor().unwrap();
+
+    let quarter_sec = time::Duration::from_millis(250);
+
+    loop {
+        let size = terminal.size().unwrap();
+        if size != interface.size {
+            terminal.resize(size).unwrap();
+            interface.size = size;
+        }
+
+        draw(&mut terminal, &vm, &interface);
+
+        use termion::event;
+
+        let evt = rx.try_recv();
+        use std::sync::mpsc::TryRecvError::*;
+
+        match evt {
+            Ok(event::Key::Char('q')) => {
+                break;
+            }
+            Ok(_) => {}
+            Err(Disconnected) => {
+                break;
+            }
+            Err(Empty) => {}
+        }
+
+        if !vm.stopped {
+            match vm.step() {
+                Err(e) => {
+                    error!("{:?}", e);
+                    vm.stopped = true;
+                }
+                Ok(i) => {
+                    interface.past_instrs.push_front(i);
+                }
+            }
+        }
+
+        thread::sleep(quarter_sec);
     }
-  });
 
-  terminal.clear().unwrap();
-  terminal.hide_cursor().unwrap();
-
-  let quarter_sec = time::Duration::from_millis(250);
-
-  loop {
-    let size = terminal.size().unwrap();
-    if size != interface.size {
-        terminal.resize(size).unwrap();
-        interface.size = size;
-    }
-
-    draw(&mut terminal, &vm, &interface);
-
-    use termion::event;
-
-    let evt = rx.try_recv();
-    use std::sync::mpsc::TryRecvError::*;
-
-    match evt {
-      Ok(event::Key::Char('q')) => { break; }
-      Ok(_) => {}
-      Err(Disconnected) => { break; }
-      Err(Empty) => { }
-    }
-
-    if !vm.stopped {
-      match vm.step() {
-        Err(e) => { error!("{:?}", e); vm.stopped = true;},
-        Ok(i) => { interface.past_instrs.push_front(i); },
-      }
-    }
-
-    thread::sleep(quarter_sec);
-  }
-
-  terminal.show_cursor().unwrap();
+    terminal.show_cursor().unwrap();
 }
