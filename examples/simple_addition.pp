48a6a8b06c478a4f651d9b796fb11d50dc75e89b
Xavier Denis <xldenis@gmail.com>
2018-09-08 21:05:02 +0200

    Simple example programs

---
+++ examples/simple_addition.pp
@@ -0,0 +1,8 @@
+1. Variable Addresses
+2. Parameters
+3. List of Constants and Variable Quantities
+
+0, 10, 5, 1, a
+4. Logical Scheme
+
+0 + 10 => a; a + 5 => a; a - 1 => a; Form a , => 0; stop
---
+++ examples/simple_logical.pp
@@ -0,0 +1,8 @@
+1. Variable Addresses
+2. Parameters
+3. List of Constants and Variable Quantities
+
+0, 10, 5, 1, a, β
+4. Logical Scheme
+
+P(1, 0101; 0102 / (-∞, 10)); L0101 Form 1 ,=> 0; L0102 Form 10 ,=> 0
