619795cf35962bc6d50a52abe4d7655e9bec3fd3
Xavier Denis <xldenis@gmail.com>
2018-11-14 14:42:21 +0100

    Add some more example programs

---
+++ examples/commutivity.pp
@@ -0,0 +1,8 @@
+1. Variable Addresses
+2. Parameters
+3. List of Constants and Variable Quantities
+
+a, b, c
+4. Logical Scheme
+
+a * b => c ; b * a => c
---
+++ examples/simple_loop.pp
@@ -0,0 +1,13 @@
+1. Variable Addresses
+2. Parameters
+
+β : β_in = 0, β_fin = 10
+α : α_in = 0, α_fin = 10
+
+3. List of Constants and Variable Quantities
+
+a, b, c, 10, 0
+
+4. Logical Scheme
+
+[α a * b => c [β c - b => c ]]
