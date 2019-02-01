5de4a8b0e1a80b543ac4a1f54c8ec004ff0cf91b
Xavier Denis <xldenis@gmail.com>
2019-02-01 08:53:22 +0100

    Extract a common readfile function

--- besm-vm/src/opt.rs
+++ besm-vm/src/opt.rs
@@ -52,37 +52,12 @@ use vm::mag::MagDrive;
 
 pub fn md_from_file(file: Option<PathBuf>) -> MagDrive {
     let mut buf = [0; 1024];
+
     match file {
         None => {}
-        Some(path) => {
-            let mut f = File::open(&path).expect("file not found");
-
-            match path.extension().and_then(OsStr::to_str) {
-                Some("txt") => {
-                    use std::io::BufReader;
-                    use std::io::BufRead;
-                    let ls = BufReader::new(&f);
-
-                    let words: Vec<u64> = ls.lines()
-                    .map(|line|
-                        line.as_ref().map(|l|
-                            u64::from_str_radix(l, 16).unwrap_or(0)
-                        ).unwrap_or(0)
-                    ).chain(iter::repeat(0))
-                    .take(1024)
-                    .collect();
-
-                    buf.copy_from_slice(&words[0..1024]);
-                }
-                Some("bin") => {
-                    let words: Vec<u64> = iter::repeat_with(|| f.read_u64::<BigEndian>().unwrap_or(0))
-                    .take(1024)
-                    .collect();
-
-                    buf.copy_from_slice(&words);
-                }
-                _ => panic!("omg")
-            }
+        Some(f) => {
+            let words = read_file(&f);
+            buf.copy_from_slice(&words[..1024])
         }
     }
 
@@ -106,9 +81,15 @@ use std::path::Path;
 use std::ffi::OsStr;
 
 pub fn is_from_file(path: &Path) -> [u64; 1023] {
-    let mut file = File::open(path).expect("file not found");
     let mut is_buf = [0u64; 1023];
 
+    let words = read_file(path);
+    is_buf.copy_from_slice(&words[..1023]);
+    is_buf
+}
+
+pub fn read_file(path: &Path) -> Vec<u64> {
+    let mut file = File::open(path).expect("file not found");
     match path.extension().and_then(OsStr::to_str) {
         Some("txt") => {
             use std::io::BufReader;
@@ -120,19 +101,17 @@ pub fn is_from_file(path: &Path) -> [u64; 1023] {
                 line.as_ref().map(|l|
                     u64::from_str_radix(l, 16).unwrap_or(0)
                 ).unwrap_or(0)
-            ).take(1023)
+            )
             .collect();
 
-            is_buf.copy_from_slice(&words[..]);
+            return words;
         }
         Some("bin") => {
            let words: Vec<u64> = iter::repeat_with(|| file.read_u64::<BigEndian>().unwrap_or(0))
-           .take(1023)
            .collect();
 
-           is_buf.copy_from_slice(&words[..]);
+           return words;
        }
        _ => { panic!("unsupported is file type.");}
    }
-   is_buf
 }
