8bdb7201b86ef7a58201e5ab8be1dd450f8da0fa
Xavier Denis <xldenis@gmail.com>
2019-06-02 16:22:15 +0200

    Commit state of generated files

---
+++ examples/variable_address.pp
@@ -0,0 +1,17 @@
+1. Variable Addresses
+
+(a) Block a (10 cells)
+
+<a_α> = 1.α
+
+2. Parameters
+
+α : α_in = 0, α_fin = 10
+
+3. List of Constants and Variable Quantities
+
+a, b, c, 10, 0
+
+4. Logical Scheme
+
+[α 10 => a_α ]
